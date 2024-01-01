260225ae748a355e894fa709410889232f35f814
Ademola Fadumo <demolafadumo@gmail.com>
2024-01-01 17:33:16 +0100

    Initial Commit

---
+++ check_it/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  check_it
+//
+//  Created by Ademola Fadumo on 01/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
